64acdcc242b0652035641587082602a3849f2f49
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-06-12 22:09:29 +0530

    fix: red border landing page removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,32 +10,32 @@ export default async function Home() {
   const session = await auth();
 
   return (
-    <main className="bg-background text-foreground border-red flex h-screen w-full flex-col items-center justify-evenly border-2 border-red-600 p-5">
+    <main className="border-red flex h-screen w-full flex-col items-center justify-evenly border-2 bg-background p-5 text-foreground">
       <div className="flex flex-col items-center gap-3 text-center">
         <p className="text-5xl font-medium">Styles</p>
-        <p className="text-foreground2 text-xl">
+        <p className="text-xl text-foreground2">
           Edit styles/globals.css to make changes
         </p>
       </div>
 
       <div className="flex w-[140px] flex-col items-center gap-2">
         <p className="text-foreground2">Background</p>
-        <div className="bg-background border-border h-[60px] w-full rounded-sm border-2" />
+        <div className="h-[60px] w-full rounded-sm border-2 border-border bg-background" />
       </div>
 
       <div className="flex w-[140px] flex-col items-center gap-2">
         <p className="text-foreground2">Card</p>
-        <div className="bg-card border-border h-[60px] w-full rounded-sm border" />
+        <div className="h-[60px] w-full rounded-sm border border-border bg-card" />
       </div>
 
       <div className="flex w-[140px] flex-col items-center gap-2">
         <p className="text-foreground2">border</p>
-        <div className="bg-border border-border h-[60px] w-full rounded-sm border" />
+        <div className="h-[60px] w-full rounded-sm border border-border bg-border" />
       </div>
 
       <div className="flex w-[140px] flex-col items-center gap-2">
         <p className="text-foreground2">primary</p>
-        <div className="bg-primary border-border h-[60px] w-full rounded-sm border" />
+        <div className="h-[60px] w-full rounded-sm border border-border bg-primary" />
       </div>
     </main>
   );
