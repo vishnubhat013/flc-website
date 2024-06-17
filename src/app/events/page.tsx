f58152e6112fb0e9d3333f18fd147ab4f88c6f5a
Vishnu Bhat <vishnubhat013@gmail.com>
2024-06-17 21:34:02 +0530

    style:event page radio buttons

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "~/components/ui/Card";
-import RadioButtons from "~/components/RadioButtons";
+import RadioButtons from "~/components/ui/RadioButtons";
 function page() {
   return (
     <>
